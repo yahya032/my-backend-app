2058a149f9e4fbcaa2bbb15d1cb9948f364a6ad0
yahya032 <22032@supnum.mr>
2026-01-27 21:11:57 +0100

    Configure Google Cloud Storage pour médias et fichiers PDFs

--- python_project/api_urls.py
+++ python_project/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
+from python_project.admin_site import get_admin_site
+from python_project.views import (
     UniversityViewSet,
     SpecialityViewSet,
     LevelViewSet,
@@ -8,13 +9,14 @@ from .views import (
     MatiereViewSet,
     DocumentViewSet,
     list_firebase_users,
-    create_firebase_user
+    create_firebase_user,
+    user_alerts,
 )
 from django.conf import settings
 from django.conf.urls.static import static
-from python_project.admin_site import admin_site
 
-# Router DRF
+admin_site = get_admin_site()  # ⚠️ important
+
 router = DefaultRouter()
 router.register(r'universities', UniversityViewSet, basename='university')
 router.register(r'specialities', SpecialityViewSet, basename='speciality')
@@ -23,13 +25,12 @@ router.register(r'semesters', SemesterViewSet, basename='semester')
 router.register(r'matieres', MatiereViewSet, basename='matiere')
 router.register(r'documents', DocumentViewSet, basename='document')
 
-# URL patterns
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('api/', include(router.urls)),
     path('api/firebase-users/', list_firebase_users, name='firebase-users'),
     path('api/firebase-create-user/', create_firebase_user, name='firebase-create-user'),
+    path('api/alerts/', user_alerts, name='user-alerts'),
 ]
 
-# Servir les fichiers médias en DEV et PROD
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
