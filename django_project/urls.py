0290b562da3aa5b1d4fc52202620c407f4bf0482
yahya032 <22032@supnum.mr>
2026-01-27 22:15:54 +0100

    Fix admin_site import, update api_urls, minimal custom dashboard template

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('python_project.api_urls')),  # inclut toutes les URLs de l'app
+    path('admin/', admin.site.urls),  # administration
+    path('', include('python_project.api_urls')),  # inclut toutes les URLs de ton app
 ]
+
+# Sert les fichiers media en DEV
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
